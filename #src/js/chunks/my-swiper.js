08f41d9fa27353c1854a7de5373f8bbf73b4a5e5
Artem <darkoto12@gmail.com>
2023-08-01 15:26:47 +0300

    first commit

---
+++ #src/js/chunks/my-swiper.js
@@ -0,0 +1,11 @@
+new Swiper(".kitchen__slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: true,
+  navigation: {
+    nextEl: ".kitchen__next",
+    prevEl: ".kitchen__prev",
+  },
+});
+
